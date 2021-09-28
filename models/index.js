c9fc3c9c39753c3a810a0f865bc1aed1071f6a29
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-28 18:16:32 -0400

    Finish building out relationships in /models/index.js

--- models/index.js
+++ models/index.js
@@ -3,19 +3,35 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 Post.belongsTo(User, {
-  foreignKey: 'userId',
+  foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'postId',
+  foreignKey: 'post_id',
   onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'userId',
+  foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+
+
+
 
 module.exports = {
   User,
