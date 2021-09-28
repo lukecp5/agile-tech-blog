57f0302b9050e5ce4dc1c6b88d5b4c1211113fee
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-28 18:13:53 -0400

    Finish building out Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -5,10 +5,37 @@ class Comment extends Model {}
 
 Comment.init(
   {
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
+    {
+      id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'user',
+            key: 'id'
+          }
+        },
+        post_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'post',
+            key: 'id'
+          }
+        },
+        body: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1]
+          }
+        }
+    },
   },
   {
     sequelize,
