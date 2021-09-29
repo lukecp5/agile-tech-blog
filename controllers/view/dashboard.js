a9d5bc4a740770bcb86b6d76f1463423b790a36b
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-29 18:19:42 -0400

    Add form to add a new blog post from the dashboard. Add Front-end Javascript code to handle the form submission on the create post page

--- controllers/view/dashboard.js
+++ controllers/view/dashboard.js
@@ -41,6 +41,42 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
-
+router.get('/create/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // + Use the ID from the session to decipher the user that's logged in when creating posts
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'post_content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('blogNew', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router
