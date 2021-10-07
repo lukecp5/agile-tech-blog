23e763eff6fbb7a4b45b6f911ab2e29724caf01a
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-07 18:43:46 -0400

    Migrate User routes to a user-routes.js file from auth.js. Auth.js was getting too crowded and the name wasn't descriptive enough for me to easily identify where I needed to go for troubleshooting.

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,33 @@
+// Dependencies
+// Express.js connection
+const router = require('express').Router();
+// User, Post, Vote models
+const { User, Post, Comment } = require('../../models');
+// Express Session for the session data
+const session = require('express-session');
+// Authorization Helper
+const withAuth = require('../../utils/auth');
+// Sequelize store to save the session so the user can remain logged in
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Routes
+
+// GET /api/users -- get all users
+router.get('/', (req, res) => {
+    // Access the User model and run .findAll() method to get all users
+    User.findAll({
+        // when the data is sent back, exclude the password property
+        attributes: { exclude: ['password'] }
+    })
+      // return the data as JSON formatted
+      .then(dbUserData => res.json(dbUserData))
+      // if there is a server error, return that error
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+
+module.exports = router;
