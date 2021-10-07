8a7bb58b2ee07340589b43d2ce3a3002079a08ba
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-07 18:41:40 -0400

    Create route to update specific blog posts by their ID. I send the ID from the front end when a user clicks on a specific blog post.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -108,5 +108,26 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
+// PUT api/posts/1-- update a post's title or text
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(req.body,
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
 
 module.exports = router;
