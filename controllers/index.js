8f6940a8ef5d8464aaaab90193f6b36faf485ae9
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-07 18:32:41 -0400

    Rearrange routes for the dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,27 @@
-const router = require("express").Router();
+// An index file to gather the routes to export to the server
 
-const homeRoutes = require("./view/home");
-const authRoutes = require("./view/auth");
-const dashboardRoutes = require("./view/dashboard");
-const blogRoutes = require("./view/blog");
-const apiAuthRoutes = require("./api/auth");
-const apiBlogRoutes = require("./api/blog");
-const apiCommentRoutes = require("./api/comment");
+// Dependencies
+// Server connection
+const router = require('express').Router();
+// API routes folder
+const apiRoutes = require('./api');
+// Homepage routes
+const homeRoutes = require('./home-routes.js');
+// Dashboard Routes
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use("/", homeRoutes);
-router.use("/", authRoutes);
-router.use("/dashboard", dashboardRoutes);
-router.use("/blogs", blogRoutes);
-router.use("/api", apiAuthRoutes);
-router.use("/api/blogs", apiBlogRoutes);
-router.use("/api/comments", apiCommentRoutes);
+// Define the path for the server for the API routes
+router.use('/api', apiRoutes);
+
+// Define the path for the home page
+router.use('/', homeRoutes);
+
+// Define the path for the dashboard
+router.use('/dashboard', dashboardRoutes);
+
+// Define a catch-all route for any resource that doesn't exist
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
