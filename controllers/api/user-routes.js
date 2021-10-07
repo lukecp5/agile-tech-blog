4ae02bf3cb6292a2c86ea8f66c75740e60fbee34
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-07 18:45:48 -0400

    Move Login API endpoint to the user-routes.js file. Fix glitches with the user not actually being logged in when they pass correct username and password into the login form

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -54,5 +54,38 @@ router.post('/', (req, res) => {
     });
 });
 
+// POST /api/users/login -- login route for a user
+router.post('/login',  (req, res) => {
+    // findOne method by email to look for an existing user in the database with the email address entered
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+        email: req.body.email
+        }
+    }).then(dbUserData => {
+        // if the email is not found, return an error
+        if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+        }
+        // Otherwise, verify the user.
+        // call the instance method as defined in the User model
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        // if the password is invalid (method returns false), return an error
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        // otherwise, save the session, and return the user object and a success message
+        req.session.save(() => {
+          // declare session variables
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });  
+});
 
 module.exports = router;
