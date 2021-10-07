ee3f9149360e5bf03864cbdc9ea82db8ae753f1f
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-07 18:28:55 -0400

    Finish off the Post model to have all of the required properties/attributes

--- models/Post.js
+++ models/Post.js
@@ -5,11 +5,37 @@ class Post extends Model {}
 
 Post.init(
   {
-    title: DataTypes.TEXT,
-    content: DataTypes.TEXT
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      title: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      content: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          validate: {
+              // post must be at least one character long
+              len: [1]
+          }
+      },
+      user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: 'user',
+              key: 'id'
+          }
+      }
   },
   {
-    sequelize
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
   }
 );
 
